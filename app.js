a5e63401e01c0a671e1c92b56b9b579921124d15
paulsiskind <paulsiskind@gmail.com>
2016-01-07 10:21:23 -0700

    added mongodb to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
